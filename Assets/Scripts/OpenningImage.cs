5f63db91137bb0b403252023c001aafd01c21a31
ZackaryValenta <zackary.valenta@gmail.com>
2015-06-16 18:19:00 -0400

    Made opening scene UI dynamically adjust to screen size
    - title and buttons anchored to never exit screen
    - background resizes to screen dimension while keeping texture ratio

---
+++ Assets/Scripts/OpenningImage.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OpenningImage : MonoBehaviour
+{
+	[SerializeField]
+	Camera mainCamera;
+
+	private Image image;
+	private float textureWidthHeightRatio;
+
+	// Use this for initialization
+	void Start ()
+	{
+		image = gameObject.GetComponent<Image> ();
+		textureWidthHeightRatio = image.mainTexture.width / image.mainTexture.height;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float screenWidth = Screen.width;
+		float screenHeight = Screen.height;
+		float newWidth;
+		float newHeight;
+
+		if ((screenWidth / textureWidthHeightRatio) >= screenHeight)
+		{
+			newWidth = screenWidth;
+			newHeight = screenWidth / textureWidthHeightRatio;
+		}
+		else
+		{
+			newHeight = screenHeight;
+			newWidth = screenHeight * textureWidthHeightRatio;
+		}
+		image.rectTransform.sizeDelta = new Vector2 (newWidth, newHeight);
+	}
+}
