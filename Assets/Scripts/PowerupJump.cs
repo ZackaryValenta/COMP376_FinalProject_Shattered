292c4072a0b9b2b2cd3db8043878cdc523b87ed4
SkylerWittman <skyler.wittman@gmail.com>
2015-06-15 20:59:29 -0400

    Powerup scripts are uploaded

---
+++ Assets/Scripts/PowerupJump.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupJump : MonoBehaviour {
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.CompareTag("Player"))
+		{
+			col.gameObject.GetComponent<Player>().setDoubleJumpUnlocked(true);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PowerupStrength.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupStrength : MonoBehaviour {
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.CompareTag("Player"))
+		{
+			col.gameObject.GetComponent<Player>().setSpinKickUnlocked(true);
+			Destroy (gameObject);
+		}
+	}
+}
