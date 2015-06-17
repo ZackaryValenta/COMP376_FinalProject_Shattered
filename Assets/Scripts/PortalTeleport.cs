83fe78a2861e04b06c094cf7d096b8e3685be4f8
SkylerWittman <skyler.wittman@gmail.com>
2015-06-17 17:09:13 -0400

    Added bossScene to prefab

---
+++ Assets/Scripts/PortalTeleport.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalTeleport : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col)	
+	{
+		if (col.gameObject.CompareTag ("Player")) 
+		{
+			Application.LoadLevel("BossScene");
+		}
+	}
+}
